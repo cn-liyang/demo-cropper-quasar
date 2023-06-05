33386733cfe8a496fa17f07fcf35d6c0925686e3
李扬 <foryli@qq.com>
2023-06-05 14:25:07 +0800

    wip

--- src/components/CropperDialog.vue
+++ src/components/CropperDialog.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <CropperDialogLayout title="图片剪裁">
+  <DialogLayout title="图片剪裁">
     <CropperDialogImage />
     <CropperDialogSticky />
-  </CropperDialogLayout>
+  </DialogLayout>
 </template>
